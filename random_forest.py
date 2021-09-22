4d839d284ecc94fc2ed4c12e6bd9596a513c3526
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2021-09-22 15:48:24 +1000

    Update random_forest.py

--- random_forest.py
+++ random_forest.py
@@ -1,6 +1,3 @@
-#### Sanaa Hobeichi's code
-
-
 import tensorflow as tf
 import pandas as pd
 import numpy as np
@@ -9,126 +6,64 @@ import tensorflow_decision_forests as tfdf
 from wurlitzer import sys_pipes
 import time
 import netCDF4 as nc
-import os
+
 from numpy.random import rand
+import os, psutil
+from datetime import datetime
 
-print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+startTime = datetime.now()
 
-models = ['CanESM5', 'CESM2-WACCM','CMCC-CM2-SR5', 'EC-Earth3', 'EC-Earth3-Veg']
+models = ['CanESM5', 'CESM2-WACCM','CMCC-CM2-SR5', 'EC-Earth3', 'EC-Earth3-Veg',
+          'GFDL-CM4', 'IITM-ESM', 'INM-CM4-8', 'INM-CM5-0', 'IPSL-CM6A-LR',
+          'KIOST-ESM', 'MIROC6', 'MPI-ESM1-2-HR', 'MPI-ESM1-2-LR', 'MRI-ESM2-0',
+          'NorESM2-LM', 'NorESM2-MM']
+
+target = 'CRUJRA'
+
 a = np.random.randn(20)
 b = np.random.randn(20)
 c = np.random.randn(20)
 d = np.random.randn(20)
-d = np.random.randn(20)
 e = np.random.randn(20)
+f = np.random.randn(20)
+g = np.random.randn(20)
+h = np.random.randn(20)
+i = np.random.randn(20)
+j = np.random.randn(20)
+k = np.random.randn(20)
+l = np.random.randn(20)
+m = np.random.randn(20)
+n = np.random.randn(20)
+o = np.random.randn(20)
+p = np.random.randn(20)
+q = np.random.randn(20)
 
-fake_data = [a,b,c,d,e]
-train_df = pd.DataFrame()
+fake_data_train = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p]
+full_df = pd.DataFrame()
+for m,fd in zip(models,fake_data_train):
 
-for m,fd in zip(models,fake_data):
-    train_df[m] = fd
+    full_df[m] = fd
 
-train_df['CRUJRA'] = e
+full_df[target] = q
 
-print(train_df.head())
+train_df = full_df.head(15)
+test_df = full_df.tail(5)
 
 # Convert my training data to tensorflow objects
 train_ds = tfdf.keras.pd_dataframe_to_tf_dataset(train_df, label='CRUJRA', task=tfdf.keras.Task.REGRESSION)
-print(train_ds)
 
 # Configure the model.
-model1 = tfdf.keras.RandomForestModel(task = tfdf.keras.Task.REGRESSION, num_candidate_attributes=3, num_trees = 10)
+model1 = tfdf.keras.RandomForestModel(task = tfdf.keras.Task.REGRESSION, num_candidate_attributes=5, num_trees = 100)
 model1.compile(metrics=["mse"])
-print(model1)
 
 # Train the model.
 start_time = time.time()
-with sys_pipes():
-    model1.fit(x=train_ds)
+model1.fit(x=train_ds)
+file_name = "model"
 
-file_name = "model.csv"
 model1.save(file_name)
 end_time = time.time()
 
-time_need = end_time - start_time
-print("time needed to run 10 forest is {}".format(time_need))
-
-# ##############################################################################################
-# ##############################################################################################
-
-# ###################################################################################################################
-# ###################################################################################################################
-# #%% TESTING
-# for bin in range(9,21):
-#   print(bin)
-#   file_name = "/mnt/e/Data/flux_Gab/half_hourly/csv/bins/test_bin{}.csv".format(bin)
-#   test_site = pd.read_csv(file_name, index_col=0)
-
-#   ## Prepare test dataset
-#   test_df = pd.DataFrame()
-#   sites_df = pd.DataFrame()
-#   i = 0
-#   for s in test_site.site_code:
-#       print(i)
-#       file_name = "/mnt/e/Data/flux_Gab/half_hourly/csv/sites_testing_Qle/{}_testing_half_hourly.csv".format(s)
-#       s_df =  pd.read_csv(file_name, index_col=0)
-#       #s_df = s_df.dropna()
-#       s2_df = pd.DataFrame( data={'site_code':s_df['site_code']})
-#       sites_df = sites_df.append(s2_df,ignore_index=True)
-
-#       s_df = s_df.drop('site_code', 1)
-#       test_df = test_df.append(s_df, ignore_index=True)
-
-#       i = i+1
-
-#   ## Convert the testing dataset into tensorflow object
-#   test_ds = tfdf.keras.pd_dataframe_to_tf_dataset(test_df, label='Qle', task=tfdf.keras.Task.REGRESSION)
-
-#   ## Load the saved model
-#   file_name = "/mnt/e/Data/flux_Gab/half_hourly/RFTF/model_bin{}.csv".format(bin)
-#   model2 = tf.keras.models.load_model(file_name)
-
-#   pred = model2.predict(test_ds)
-
-#   ## save the results as .csv
-#   pred_df = pd.DataFrame(pred, columns=['pred'])
-#   obs_df = pd.DataFrame( data={'obs':test_df['Qle']})
-#   pred_df = pd.concat([sites_df, pred_df, obs_df], axis=1)
-#   pred_df.insert(0, "bin", bin)
-
-#   file_name = file_name = "/mnt/e/Data/flux_Gab/half_hourly/csv/bins/pred_bin{}.csv".format(bin)
-#   pred_df.to_csv(file_name, index=False)
-
-
-# ############################################################################################
-# ############################################################################################
-
-# #%% EVALUATION -- compare predicted Qle with the Observed Qle
-# evaluation =model2.evaluate(test_ds, return_dict=True)
-# print(evaluation)
-# print(f"MSE:".format(evaluation['mean_squared_error']))
-# #%%
-# print(np.mean(pred))
-# print(np.mean(test_df['Qle']))
-
-# print(np.std(pred))
-# print(np.std(test_df['Qle']))
-
-# print(np.median(pred))
-# print(np.median(test_df['Qle']))
-
-# print(type(pred))
-
-# ff = test_df['Qle'].to_numpy(dtype=type(pred))
-# print(type(ff))
-# print(ff[0])
-# print(pred)
-# print(np.corrcoef(pred, ff))
-
-# #%%
-# ## USE Gradient Boosted Tree Models from Tensorflow to test the importance of each feature
-# # train_rank_ds = tfdf.keras.pd_dataframe_to_tf_dataset(train_df, label='Qle', task=tfdf.keras.Task.RANKING)
-# # model_rank = tfdf.keras.GradientBoostedTreesModel( task=tfdf.keras.Task.RANKING,ranking_group="climate", num_trees=50)
-
-# # with sys_pipes():
-# #   model_rank.fit(x=train_rank_ds)
+process = psutil.Process(os.getpid())
+print(process.memory_info().rss/(1024 ** 2))
+print(datetime.now() - startTime)
