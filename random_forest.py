fd5aefd7b8249cb52be124e6fdc7305b415714a8
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2021-09-22 16:13:00 +1000

    Update random_forest.py

--- random_forest.py
+++ random_forest.py
@@ -1,3 +1,7 @@
+'''
+Based on Sanaa Hobeichi's script
+'''
+
 import tensorflow as tf
 import pandas as pd
 import numpy as np
@@ -13,13 +17,16 @@ from datetime import datetime
 
 startTime = datetime.now()
 
+### names of CMIP6 models
 models = ['CanESM5', 'CESM2-WACCM','CMCC-CM2-SR5', 'EC-Earth3', 'EC-Earth3-Veg',
           'GFDL-CM4', 'IITM-ESM', 'INM-CM4-8', 'INM-CM5-0', 'IPSL-CM6A-LR',
           'KIOST-ESM', 'MIROC6', 'MPI-ESM1-2-HR', 'MPI-ESM1-2-LR', 'MRI-ESM2-0',
           'NorESM2-LM', 'NorESM2-MM']
 
+### name of target (reanalysis)
 target = 'CRUJRA'
 
+### generate fake data
 a = np.random.randn(20)
 b = np.random.randn(20)
 c = np.random.randn(20)
@@ -39,24 +46,25 @@ p = np.random.randn(20)
 q = np.random.randn(20)
 
 fake_data_train = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p]
+
 full_df = pd.DataFrame()
 for m,fd in zip(models,fake_data_train):
     full_df[m] = fd
 
 full_df[target] = q
 
+### Split into to train and test data
 train_df = full_df.head(15)
 test_df = full_df.tail(5)
 
-# Convert my training data to tensorflow objects
+### Convert training data to tensorflow objects
 train_ds = tfdf.keras.pd_dataframe_to_tf_dataset(train_df, label='CRUJRA', task=tfdf.keras.Task.REGRESSION)
 
-# Configure the model.
+### Configure the model
 model1 = tfdf.keras.RandomForestModel(task = tfdf.keras.Task.REGRESSION, num_candidate_attributes=5, num_trees = 100)
 model1.compile(metrics=["mse"])
 
-# Train the model.
-start_time = time.time()
+### Train the model
 model1.fit(x=train_ds)
 file_name = "model"
 
@@ -64,5 +72,13 @@ model1.save(file_name)
 end_time = time.time()
 
 process = psutil.Process(os.getpid())
+
+### Get memory
 print(process.memory_info().rss/(1024 ** 2))
+## Get run time
 print(datetime.now() - startTime)
+
+#  TESTING
+# Convert the testing dataset into tensorflow object
+test_ds = tfdf.keras.pd_dataframe_to_tf_dataset(test_df, label='CRUJRA', task=tfdf.keras.Task.REGRESSION)
+pred = model1.predict(test_ds)
